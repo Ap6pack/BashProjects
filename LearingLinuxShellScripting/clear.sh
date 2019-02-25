073ce8121b18616ff79170faf7974290bb357555
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2019-02-25 05:25:36 -0500

    Clears the window greets the user and displays the current date and time

---
+++ LearingLinuxShellScripting/clear.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script clears the window, greets the user,
+# and displays the current date and time.
+
+clear # Clear the window
+
+echo "SCRIPT BEGINS"
+echo "Hello $LOGNAME!" # Greet the user
+echo
+echo "Today's date and time:"
+date # Display current date and time
+echo # Will print empty line
+my_num=50
+my_day="Sunday"
+echo "The value of my_num is $my_num"
+echo "The value of my_day is $my_day"
+echo
+echo "SCRIPT FINISHED!!"
+echo
