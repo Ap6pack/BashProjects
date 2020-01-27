cf378a38ff30cb6cef147bc15d2616405e72f79e
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2020-01-26 21:26:40 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 echo updating....
 sleep 5s
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt-get autoremove -y
+service tor restart && service privoxy restart
+apt update
+apt upgrade -y
+apt dist-upgrade -y
+apt autoremove -y
 echo completed
-
 clear
+vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000 -o nonempty
+ifconfig eth0 down
+macchanger -a -A eth0
+macchanger -a -A eth0
+macchanger -a -A eth0
+ifconfig eth0 up
