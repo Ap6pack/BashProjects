2caccf49da826a0b355f180d18c9b34c6d5bb4d4
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2020-04-06 13:10:07 -0400

    minor change
    Add the following line: /etc/init.d/dnsmasq restart

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo updating....
 sleep 5s
+/etc/init.d/dnsmasq restart
 service tor restart && service privoxy restart
 apt update
 apt upgrade -y
 apt dist-upgrade -y
 apt autoremove -y
 echo completed
+sleep 5s
 clear
 vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000 -o nonempty
 ifconfig eth0 down
