88bdb566e1324fe071e368a63354c57dffbe8494
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2020-09-16 13:39:06 -0400

    Update update.sh
    minor update

--- update.sh
+++ update.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 echo updating....
-sleep 5s
-/etc/init.d/dnsmasq restart
-service tor restart && service privoxy restart
-apt update
-apt upgrade -y
-apt dist-upgrade -y
-apt autoremove -y
-echo completed
-sleep 5s
-clear
-vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000 -o nonempty
+
 ifconfig eth0 down
 macchanger -a -A eth0
 macchanger -a -A eth0
 macchanger -a -A eth0
 ifconfig eth0 up
+sleep 5s
+
+anonsurf start
+anonsurf change
+anonsurf myip
+sleep 5s
+
+apt update
+apt full-upgrade -y
+apt dist-upgrade -y
+apt autoremove -y
+echo completed
+
+sleep 20s
+clear
+
+vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000 -o nonempty
